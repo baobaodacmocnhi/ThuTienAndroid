bf5b56cb654ee5b667f2609d33b4c08ef74af5e9
BaoBao <baobaodacmocnhi@gmail.com>
2019-08-01 16:56:01 +0700

    update

--- app/src/main/java/vn/com/capnuoctanhoa/thutienandroid/LenhHuy/ActivityLenhHuy.java
+++ app/src/main/java/vn/com/capnuoctanhoa/thutienandroid/LenhHuy/ActivityLenhHuy.java
@@ -1,15 +1,194 @@
 package vn.com.capnuoctanhoa.thutienandroid.LenhHuy;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import vn.com.capnuoctanhoa.thutienandroid.Class.CLocal;
+import vn.com.capnuoctanhoa.thutienandroid.Class.CWebservice;
+import vn.com.capnuoctanhoa.thutienandroid.HanhThu.ActivityDownDataHanhThu;
 import vn.com.capnuoctanhoa.thutienandroid.R;
 
 public class ActivityLenhHuy extends AppCompatActivity {
+    private Button  btnTimKiem,btnCapNhat,btnShowMess;
+    private RadioButton radCatTam,radCatHuy;
+    private CheckBox chkCat;
+    private EditText edtMa,edtTinhTrang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lenh_huy);
+
+        radCatTam = (RadioButton) findViewById(R.id.radCatTam);
+        radCatHuy = (RadioButton) findViewById(R.id.radCatHuy);
+        chkCat = (CheckBox) findViewById(R.id.chkCat);
+        edtMa= (EditText) findViewById(R.id.edtMa);
+        edtTinhTrang= (EditText) findViewById(R.id.edtTinhTrang);
+        btnTimKiem = (Button) findViewById(R.id.btnTimKiem);
+        btnCapNhat = (Button) findViewById(R.id.btnCapNhat);
+        btnShowMess = (Button) findViewById(R.id.btnShowMess);
+
+        btnTimKiem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (CLocal.checkNetworkAvailable(getApplicationContext()) == false) {
+                    Toast.makeText(getApplicationContext(), "Không có Internet", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                ActivityLenhHuy.MyAsyncTask myAsyncTask = new ActivityLenhHuy.MyAsyncTask();
+                myAsyncTask.execute("TimKiem");
+            }
+        });
+
+        btnCapNhat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (CLocal.checkNetworkAvailable(getApplicationContext()) == false) {
+                    Toast.makeText(getApplicationContext(), "Không có Internet", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                ActivityLenhHuy.MyAsyncTask myAsyncTask = new ActivityLenhHuy.MyAsyncTask();
+                myAsyncTask.execute();
+            }
+        });
+
+        btnShowMess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builderSingle = new AlertDialog.Builder(ActivityLenhHuy.this);
+                builderSingle.setIcon(R.mipmap.ic_launcher);
+                builderSingle.setTitle("Tin nhắn đã nhận");
+                builderSingle.setCancelable(false);
+
+                ListView lstMessage = new ListView(getApplicationContext());
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(ActivityLenhHuy.this, android.R.layout.select_dialog_singlechoice);
+
+                try {
+                    if (CLocal.jsonMessage != null && CLocal.jsonMessage.length() > 0) {
+                        int stt = 0;
+                        for (int i = 0; i < CLocal.jsonMessage.length(); i++) {
+                            JSONObject jsonObject = CLocal.jsonMessage.getJSONObject(i);
+                            arrayAdapter.add(jsonObject.getString("NgayNhan") + " - " + jsonObject.getString("Title") + " - " + jsonObject.getString("Content"));
+                        }
+                    }
+                } catch (Exception ex) {
+                }
+
+                lstMessage.setAdapter(arrayAdapter);
+                builderSingle.setView(lstMessage);
+
+                builderSingle.setNegativeButton(
+                        "Thoát",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                builderSingle.setPositiveButton(
+                        "Xóa Tất Cả",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(ActivityLenhHuy.this);
+                                builder.setMessage("Bạn có chắc chắn xóa?")
+                                        .setCancelable(false)
+                                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                CLocal.jsonMessage = new JSONArray();
+                                            }
+                                        })
+                                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                dialog.cancel();
+                                            }
+                                        });
+                                AlertDialog alert = builder.create();
+                                alert.show();
+                            }
+                        });
+
+                //hàm này khi click row sẽ bị ẩn
+                /*builderSingle.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String strName = arrayAdapter.getItem(which);
+                        AlertDialog.Builder builderInner = new AlertDialog.Builder(ActivityDanhSachHanhThu3.this);
+                        builderInner.setMessage(strName);
+                        builderInner.setTitle("Your Selected Item is");
+                        builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        builderInner.show();
+                    }
+                });*/
+
+                final Dialog dialog = builderSingle.create();
+                builderSingle.show();
+            }
+        });
     }
+
+    public class MyAsyncTask extends AsyncTask<String, String, Boolean> {
+        ProgressDialog progressDialog;
+        CWebservice ws = new CWebservice();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(ActivityLenhHuy.this);
+            progressDialog.setTitle("Thông Báo");
+            progressDialog.setMessage("Đang xử lý...");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... voids) {
+            try {
+
+                return false;
+            } catch (Exception ex) {
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+            }
+            if (aBoolean == true) {
+                Intent returnIntent = new Intent();
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
+            } else {
+
+            }
+        }
+    }
+
 }
+
