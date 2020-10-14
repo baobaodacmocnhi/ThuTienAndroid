da08a955f7ea2845207b0247902d755ac3f20ac0
BaoBao <baobaodacmocnhi@gmail.com>
2020-10-14 08:44:06 +0700

    update

--- app/src/main/java/vn/com/capnuoctanhoa/thutienandroid/DongNuoc/ActivityDownDataDongNuoc.java
+++ app/src/main/java/vn/com/capnuoctanhoa/thutienandroid/DongNuoc/ActivityDownDataDongNuoc.java
@@ -429,10 +429,10 @@ public class ActivityDownDataDongNuoc extends AppCompatActivity {
                                 JSONObject jsonObjectChild = CLocal.jsonDongNuocChild.getJSONObject(k);
                                 if (jsonObjectChild.getString("MaDN").equals(enParent.getID()) == true) {
 
-                                    if (jsonObject.has("CuaHangThuHo1") == true)
-                                        enParent.setCuaHangThuHo1(jsonObject.getString("CuaHangThuHo1").replace("null", ""));
-                                    if (jsonObject.has("CuaHangThuHo2") == true)
-                                        enParent.setCuaHangThuHo2(jsonObject.getString("CuaHangThuHo2").replace("null", ""));
+                                    if (jsonObjectChild.has("CuaHangThuHo1") == true)
+                                        enParent.setCuaHangThuHo1(jsonObjectChild.getString("CuaHangThuHo1").replace("null", ""));
+                                    if (jsonObjectChild.has("CuaHangThuHo2") == true)
+                                        enParent.setCuaHangThuHo2(jsonObjectChild.getString("CuaHangThuHo2").replace("null", ""));
 
                                     CEntityChild enChild = new CEntityChild();
                                     enChild.setModifyDate(enParent.getModifyDate());
